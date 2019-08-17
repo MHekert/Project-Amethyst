385069bf9f1518c5233dddf731c7c1ab4a7207a1
MHekert <mateusz.hekert@gmail.com>
2019-08-17 17:17:06 +0200

    small bug fix

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,6 @@ import revisionController from './controllers/revision';
 import modeActionController from './controllers/modeAction';
 import authController from './controllers/auth';
 import isUserLoggedIn from './controllers/middleware/isUserLoggedIn';
-import isAuthorOfMode from './controllers/middleware/isAuthorOfMode';
 import validateRequest from './controllers/middleware/validateRequest';
 import { body } from 'express-validator/check';
 import errorHandler from './controllers/middleware/errorHandler';
@@ -13,14 +12,7 @@ const router: Router = Router();
 
 router.use('/mode', modeController);
 router.use('/revision', revisionController);
-router.use(
-	'/mode/action',
-	isUserLoggedIn,
-	[body('modeId').isHexadecimal()],
-	validateRequest,
-	isAuthorOfMode,
-	modeActionController
-);
+router.use('/mode/action', isUserLoggedIn, [body('modeId').isHexadecimal()], validateRequest, modeActionController);
 router.use('/auth', authController);
 router.use(errorHandler);
 
